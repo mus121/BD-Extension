6c0f607a16ae3f6151b2f052f49e6c8921cb8480
Abdul Basit Khan <93419176+PhoenixxBasit@users.noreply.github.com>
2024-12-23 20:26:01 +0500

    fixed message handling

--- src/services/messageHandler.ts
+++ src/services/messageHandler.ts
@@ -3,7 +3,7 @@ import { LinkedInService } from "./linkedin";
 
 export const handleMessage = async (
   message: { type: MessageType; payload?: any },
-  sendResponse: (response: any) => void,
+  sendResponse: (response: any) => void
 ) => {
   switch (message.type) {
     case MessageType.CHECK_EXTENSION: {
@@ -12,26 +12,26 @@ export const handleMessage = async (
       });
       break;
     }
-    case MessageType.FETCH_PROFILE:
+    case MessageType.FETCH_PROFILE: {
       const profileResponse = await LinkedInService.getProfile();
       sendResponse(profileResponse);
       break;
-
-    case MessageType.FETCH_CONNECTIONS:
+    }
+    case MessageType.FETCH_CONNECTIONS: {
       const connections = await LinkedInService.getConnections(
         message.payload.start,
-        message.payload.count,
+        message.payload.count
       );
       sendResponse(connections);
       break;
-
-    case MessageType.SEARCH_PROFILES:
+    }
+    case MessageType.SEARCH_PROFILES: {
       const searchResponse = await LinkedInService.searchProfiles(
-        message.payload,
+        message.payload
       );
       sendResponse(searchResponse);
       break;
-
+    }
     default:
       sendResponse({ error: { code: 400, message: "Unknown message type" } });
   }
